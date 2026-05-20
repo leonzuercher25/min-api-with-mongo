4e58865d705dca08d0c5569239af209642d6a015
your name <you@example.com>
2026-05-20 10:43:43 +0200

    Implement MongoDB integration with minimal API and update Docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS Build
 WORKDIR /src
 
-COPY ["WebApi.csproj", "./"]
-RUN dotnet restore
+COPY ["WebApi/WebApi.csproj", "WebApi/"]
+RUN dotnet restore "WebApi/WebApi.csproj"
 
 COPY . .
+WORKDIR "/src/WebApi"
 RUN dotnet publish "WebApi.csproj" -c Release -o /app/publish /p:UseAppHost=false
 
 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS final
 WORKDIR /app
-COPY --from=build /app/publish .
+COPY --from=Build /app/publish .
 
-ENV ASPNETCORE:URLS=https://+:5001
+ENV ASPNETCORE_URLS=https://+:5001
 EXPOSE 5001
 
 ENTRYPOINT [ "dotnet", "WebApi.dll" ]
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Minimal API Version 1.0.0");
+
+app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) =>
+{
+    var mongoDBConnectionString = options.Value.ConnectionString;
+    try
+    {
+        var client = new MongoClient(mongoDBConnectionString);
+        var dblist = client.ListDatabaseNames().ToList();
+        string dbs = string.Join(", ", dblist);
+        
+    return $"Zugriff auf MongoDB ok. Vorhandene DBs: {dbs}";
+    }
+    catch (Exception ex)
+    {
+        return $"Fehler beim Zugriff auf MongoDB: {ex.Message}";
+    }
+});
 
 app.Run();
